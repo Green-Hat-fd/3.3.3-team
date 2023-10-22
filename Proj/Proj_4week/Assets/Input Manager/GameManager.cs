cb00779b2857585cb26238fdbe6d102342f1fa08
Green-Hat-fd <108188257+Green-Hat-fd@users.noreply.github.com>
2023-10-22 16:10:14 +0200

    Aggiunto il nuovo InputManager

---
+++ Proj/Proj_4week/Assets/Input Manager/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public InputManager inputManager;
+    public static GameManager inst;
+
+
+    private void Awake()
+    {
+        inputManager = new InputManager();
+    }
+
+    private void OnEnable()
+    {
+        inputManager.Enable();
+
+        if (inst != null && inst != this)
+            Destroy(gameObject);
+        else
+            inst = this;
+    }
+
+    private void OnDisable()
+    {
+        inputManager.Disable();
+    }
+}
